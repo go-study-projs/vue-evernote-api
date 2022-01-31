e343006efbbcbf3468c1c6e76801e6e8cd4e3fc0
jaychenfe <chenjiehwy@163.com>
2022-01-31 10:57:56 +0800

    add CorrelationID for tracing in micro-service

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
+	"github.com/labstack/gommon/random"
 )
 
 var (
 	cfg config.Properties
+)
+
+const (
+	//CorrelationID is a request id unique to the request being made
+	CorrelationID = "X-Correlation-ID"
 )
 
 func init() {
@@ -21,7 +27,7 @@ func init() {
 func main() {
 	e := echo.New()
 	e.Logger.SetLevel(log.DEBUG)
-
+	e.Pre(addCorrelationID)
 	//jwtMiddleware := middleware.JWTWithConfig(middleware.JWTConfig{
 	//	SigningKey:  []byte(cfg.JwtTokenSecret),
 	//	TokenLookup: "header:x-auth-token",
@@ -29,4 +35,21 @@ func main() {
 
 	e.Logger.Infof("Listening on %s:%s", cfg.Host, cfg.Port)
 	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)))
+}
+
+func addCorrelationID(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// generate correlation id
+		id := c.Request().Header.Get(CorrelationID)
+		var newID string
+		if id == "" {
+			//generate a random number
+			newID = random.String(12)
+		} else {
+			newID = id
+		}
+		c.Request().Header.Set(CorrelationID, newID)
+		c.Response().Header().Set(CorrelationID, newID)
+		return next(c)
+	}
 }
